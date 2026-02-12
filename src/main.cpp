5daefc8bfb387c409881bacd077c0da890ca44ca
Rena-nf <thenamealreadytakentwice@gmail.com>
2026-02-12 07:09:54 +0700

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <atomic>
 #include <iostream>
 #include <iterator>
 #include <limits>
@@ -37,7 +38,10 @@ void reset_input()
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-
+// Karena std::string tidak memiliki to_lower
+// Maka transform digunakan dengan menagaplikasikan
+// fungsi + iterasi pada setiap character di string yang kemudian
+// Akan disimpan kembali pada string awal
 std::string to_lower_case(const std::string &str)
 {
     std::string lower = str;
@@ -45,6 +49,14 @@ std::string to_lower_case(const std::string &str)
     return lower;
 }
 
+// Lakukan looping hingga mendapatkan nilai yang sesuai
+// Validasi dilakukan dengan cara memberi batas awal dan akhir
+// Untuk memastikan bahwa input tidak keluar batas atau kosong
+// Dengan ini input yang tidak sesuai akan terdeteksi
+// Jika semua kriteria terpenuhi nilai akan direturn / kembalikan
+//
+// Prompt digunakan untuk memberikan penanda tempat menulis input sebagai
+// berikut Pilihan: <input data>
 int get_int_input(const std::string &prompt, int min, int max)
 {
     int value;
@@ -67,6 +79,8 @@ int get_int_input(const std::string &prompt, int min, int max)
     }
 }
 
+// Fungsi ini memiliki cara kerja dan logika yang sama dengan get_int_input
+// Perbedaan hanya pada tipe yang diterima dan keluarkan
 double get_double_input(const std::string &prompt, double min, double max)
 {
     double value;
@@ -89,6 +103,9 @@ double get_double_input(const std::string &prompt, double min, double max)
     }
 }
 
+// Fungsi ini memiliki logika yang hampir sama dengan get_int_input /
+// get_double_input Perbedaannya adalah pada fungsi ini hanya validasi
+// memastikan bahwa input tidak kosong
 std::string get_text_input(const std::string &prompt)
 {
     std::string input;
@@ -100,7 +117,7 @@ std::string get_text_input(const std::string &prompt)
 	    return input;
 	}
 
-	std::cout << "Error: Nama tidak boleh kosong." << "\n";
+	std::cout << "Error: Text tidak boleh kosong." << "\n";
     }
 }
 
@@ -108,11 +125,16 @@ std::string get_text_input(const std::string &prompt)
 // Semua data yang "kosong" akan memiliki nilai "" untuk string
 // Dan 0 untuk int / double
 // Saat dilakukan print / cout data yang kosong akan secara otomatis di filter
+//
+// Fungsi ini digunakan sebagai comparator / pembanding untuk sorting
+// Dimana nilai dari pointer yang dibandingkkan akan diambil kemudian
+// Nilai boolean akan dikembalikan / return
+// Ascending: kecil -> besar
 bool comp_stock_asc(Product *stock_a, Product *stock_b)
 {
     return stock_a->stock < stock_b->stock;
 }
-
+// Descending: besar -> kecil
 bool comp_stock_des(Product *stock_a, Product *stock_b)
 {
     return stock_a->stock > stock_b->stock;
@@ -120,6 +142,13 @@ bool comp_stock_des(Product *stock_a, Product *stock_b)
 
 // Pembersih console / cmd tanpa bergantung
 // Ke tipe atau jenis sistem operasi
+// Pembersih console ini menggunakan ASCII escape code
+// Untuk membersihkan layar console tanpa harus bergantung
+// Pada command / library os specific seperti
+// cls pada windows / clrscr yang merupakan bagian dari conio.h
+// Dan clear pada unix/linux
+// \033[2J akan membersihkan layar dari atas (J) ke bawah (2)
+// \033[1;1H akan mengatur posisi cursor / pointer cmd ke posisi 1 / awal prompt
 void clrscrn()
 {
     std::cout << "\033[2J\033[1;1H";
@@ -127,10 +156,22 @@ void clrscrn()
 
 class Inventaris
 {
+    // Fungsi dan properti yang tidak seharusnya diakses diluar namespace
+    // Inventaris Akan disimpan pada private
   private:
+    // m_product_init digunakan sebagai nilai initializer kepada m_products_data
+    // Yang memiliki nilai default untuk setiap tipe yang ada
     Product *m_product_init = new Product{};
     Product *m_products_data[MAX_SIZE] = {};
 
+    // Fungsi ini berbeda dengan get_text_input
+    // Karena fungsi ini memiliki tanggung jawab tambahan
+    // Yaitu memastikan bahwa id pasti unik dan tidak dipakai ulang
+    //
+    // Sama seperti get_text_input loop akan dilakukan sampai hasil yang
+    // diterima dikembalikan Tetapi akan ada nested loop yang bertugas untuk
+    // mencari semua elemen non-default di m_products_data Dan menggunakan if
+    // untuk memastikan bahwa nilai id belum digunakan
     std::string input_product_code()
     {
 	std::string input;
@@ -153,6 +194,17 @@ class Inventaris
 	return input;
     }
 
+    // Fungsi recursive ini dibuat dengan early exit
+    // Ini memastikan bahwa jika nilai kosong maka akan langsung dikirimkan 0
+    // Ini ditujukan untuk menghindari segfault karena indexing
+    //
+    // Fungsi dimulai dari akhir array yaitu [MAX_SIZE - 1]
+    // Kemudian jika pada index [0] memiliki nilai maka nilai tersebut akan
+    // ditambahkan kemudian dikembalikan
+    // Sedangkan bagian recursive terletak pada return utama dimana
+    // nilai dari harga dan stock akan dikalikan kemudian ditambahkan nilai
+    // hasil recursive dengan index - 1 hingga index mencapai 0 dimana recursive
+    // akan diakhiri
     double total_value(int n)
     {
 	double price = 0;
@@ -178,6 +230,11 @@ class Inventaris
 	return (price * stock) + total_value(n - 1);
     }
 
+    // Untuk memastikan bahwa tidak ada kesalahan karena sorting
+    // Maka fungsi ini digunakan untuk mencari semua element array
+    // Jika ada element yang tidak memiliki nilai default
+    // Maka 'false' akan dikembalikan untuk menandakan bahwa ada nilai dan array
+    // tidak kosong, jika kosong maka 'true' akan dikembalikan
     bool is_fully_empty()
     {
 	for (Product *data : m_products_data) {
@@ -189,6 +246,18 @@ class Inventaris
 	return true;
     }
 
+    // Pencarian dimulai dengan membuat string menjadi lowercase
+    // Kemudian method string find digunakan untuk mencari substring pada string
+    // Jika tidak ditemukan maka std::stirng::npos akan dikembalikan
+    // Oleh karena itu if menggunakan != std::string::npos
+    //
+    // std::string::npos adalah sebuah nilai constant yang digunakan method
+    // string seperti find dimana jika tidak ditemukan maka std::string::npos
+    // akan dikembalikan untuk menjadi penanda bahwa string tidak ditemukan
+    //
+    // Jika string ditemukan maka data akan dipilih kemudian diprint / cout
+    // dan flag is_found diset menjadi true untuk memberitahukan bahwa nilai
+    // ditemukan
     void search_by_name(std::string name)
     {
 	std::string name_lower = to_lower_case(name);
@@ -219,6 +288,8 @@ class Inventaris
 	}
     }
 
+    // Fungsi ini memiliki tujuan dan logika yang sama dengan 'search_by_name'
+    // Perbedaannya hanyalah fungsi ini menggunakan id
     void search_by_id(std::string id)
     {
 	std::string id_lower = to_lower_case(id);
@@ -249,6 +320,14 @@ class Inventaris
 	}
     }
 
+    // Fungsi ini digunakan untuk mempermudah printing / cout data
+    // Fungsi ini akan menerima paremeter Array *Product
+    // Yang kemudian akan dilakukan looping dan is_empty check dilakukan untuk
+    // Melewati nilai default
+    //
+    // Looping akan memiliki counter hingga 10 dimana pada nilai ke 10
+    // Looping akan dihentikan sementara untuk menanyakan apakah pengguna ingin
+    // melanjutkan Ini ditujukan untuk menghindari printing yang berlebihan
     void print_data(Product **arr_products_data)
     {
 	int counter = 1;
@@ -295,19 +374,35 @@ class Inventaris
 	}
     }
 
+    // Pada bagian public akan disediakan constructor dan deconstructor
+    // Pada public fungsi untuk menampilkan dan memproses menu akan diberikan
   public:
+    // Constructor dari class
     Inventaris()
     {
+	// Initialisasi semua element pada 'm_products_data' menggunakan nilai
+	// default
 	for (int i = 0; i < MAX_SIZE; i++) {
 	    m_products_data[i] = m_product_init;
 	}
     }
 
+    // Deconstructor dari class
     ~Inventaris()
     {
+	// Karena delete hanya ditujukan untuk pointer yang menggunakan new
+	// Maka 'm_products_data' tidak diperlukan untuk melakukan delete
 	delete m_product_init;
     }
 
+    // Pada fungsi ini pointer untuk data produk akan diinisialisasi
+    // Karena pointer ini akan disimpan pada array maka tidak dilakukan delete
+    // Pada akhir fungsi, ini ditujukan untuk menghindari error karena
+    // penghapusan pointer
+    //
+    // Input akan dilakukan untuk setiap input
+    // Kemudian for loop akan mencari element yang memiliki nilai default untuk
+    // di reassign / dipasangkan nlai
     void menu1_add_product()
     {
 	clrscrn();
@@ -334,6 +429,10 @@ class Inventaris
 	}
     }
 
+    // Fungsi ini akan mencari apakah nilai sudah di inisialisasi
+    // atau memiliki nilai non-default
+    // Jika iya maka akan dilakukan printing menggunakan 'print_data'
+    // Jika tidak akan diberikan peringatan lalu keluar dari fungsi
     void menu2_view_product()
     {
 	clrscrn();
@@ -349,6 +448,13 @@ class Inventaris
 	print_data(m_products_data);
     }
 
+    // Pada fungsi ini akan dilakukan check apakah ada nilai
+    // Kemudian pengguna diberikan pilihan metode pencarian
+    // Setelah dipilih pengguna akan diminta untuk memasukan data id atau nama
+    // Yang kemudian akan dikirim ke fungsi 'search_by_id' / 'search_by_name'
+    // Sesuai dengan pilihan
+    // Pengguna juga diberikan pilihan untuk keluar jika ragu
+    // atau ingin mencari nilai data terlebih dahulu
     void menu3_search_product()
     {
 	clrscrn();
@@ -383,6 +489,10 @@ class Inventaris
 	}
     }
 
+    // Sama seperti fungsi menu lainnya cek nilai akan dilakukan terlebih dahulu
+    // Kemudian jika ada nilai fungsi recursive akan dijalankan dengan index
+    // MAX_SIZE - 1, ini untuk memastikan bahwa index tidak keluar dari batas
+    // Karena index array dimulai dari 0
     void menu4_inventory_total()
     {
 	clrscrn();
@@ -400,6 +510,13 @@ class Inventaris
 		  << total_value(MAX_SIZE - 1) << std::endl;
     }
 
+    // Fungsi ini bekerja dengan flow sebagai berikut
+    // mulai -> check apakah ada nilai -> mulai looping
+    // Jika ada nilai yang kosong / default maka nilai tersebut akan dilewati
+    // Jika ditemukan nilai non-default maka nilai stock akan dicheck
+    // Untuk mencari apakah stock sudah masuk ke <= 5 atau tidak
+    // Jika iya maka nilai tersebut akan di print untuk memberitahukan
+    // pengguna
     void menu5_low_in_stock()
     {
 	clrscrn();
@@ -434,6 +551,15 @@ class Inventaris
 	}
     }
 
+    // Pertama dilakukan cek apakah nilai kosong, kemudian pengguna diberikan
+    // pilihan untuk menggunan metode ascending(Kecil -> Besar)
+    // dan Descending(Besar -> Kecil)
+    // Kemudian akan dilakukan printing data setelah sorting
+    // Fungsi ini memiliki efek samping pada ascending dimana nilai default akan
+    // Berada di depan array, ini dikarenakan bagaimana cara nilai kosong struct
+    // bekerja dimana nilai default dari stock adalah 0
+    // yang membuat nilai default
+    // Selalu menjadi nilai paling kecil
     void menu6_sort_by_stock()
     {
 	clrscrn();
@@ -468,6 +594,15 @@ class Inventaris
 	}
     }
 
+    // Fungsi ini adalah kombinasi dari menu tambah nilai dan cari dengan id
+    // Pertama dilakukan cek inisialisasi, kemudian pengguna akan dimintai id
+    // Jika sudah maka akan dilakukan pencarian
+    // jika nilai ditemukan maka akan dimintai nilai baru
+    // Nilai yang bisa diperbarui hanyalah nama, kategori, harga, dan stock
+    // Setelah itu akan dilakukan reassign pada pointer data sesuai dengan field
+    // masing - masing
+    // Jika tidak ada maka pengguna akan diberikan peringatan sebelum keluar
+    // dari fungsi
     void menu7_edit_product()
     {
 	std::cout << "===== Edit Data Produk TechMart =====" << "\n";
@@ -515,6 +650,11 @@ class Inventaris
 	}
     }
 
+    // Fungsi ini memiliki logika yang hampir sama dengan menu edit data
+    // Perbedaan utama adalah pada fungsi ini pointer data akan diisi dengan
+    // nilai default
+    // Nilai ini akan menjadi penanda is_empty == true dan
+    // akan dilewati jika dilakukan pencarian / pembacaan nilai
     void menu8_delete_data()
     {
 	std::cout << "===== Edit Data Produk TechMart =====" << "\n";
@@ -553,7 +693,14 @@ class Inventaris
     }
 };
 
-int main(int argc, char *argv[])
+// Fungsi main bekerja dengan cara melakukan inisialisasi enum class
+// Kemudian dengan constructor nilai array akan diinisialisasi
+// Setelah itu looping akan dilakukan,
+// pengguna akan dimintai pilihan menu yang ingin diakses
+// Loop hanya bisa diakhiri menggunakan return pada pilihan ke 9
+// Selain itu setelah keluar dari fungsi lainnya input dan output
+// Akan dibersihkan untuk persiapan menu selanjutnya
+int main()
 {
     Inventaris *inventarisPtr = new Inventaris;
 
